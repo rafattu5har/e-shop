34d07aeb37b04717853e8d3e28d617be4e3004a5
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-23 01:55:46 +0600

    update views for homapage

--- e_shop/e_shop/views.py
+++ e_shop/e_shop/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from category.models import Category
 
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        model_cat = Category
+        query = Category.objects.all()
+        print(query)
+        context['category_list'] = query
+        return context
