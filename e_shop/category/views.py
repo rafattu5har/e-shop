340b33f69da1a85f57a231f8480379e6d827348c
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-23 01:55:12 +0600

    creating views for category with a simple category list views in app 'category'

--- e_shop/category/views.py
+++ e_shop/category/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
+from .models import Category
 # Create your views here.
+class CatList(generic.ListView):
+    model = Category
+    template_name = 'category/category_list.html'
+    
