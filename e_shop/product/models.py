416edb3d04074639ea2d6909d1974222aed5c91e
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-22 22:25:49 +0600

    fix typos

--- e_shop/product/models.py
+++ e_shop/product/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='product_image', blank=True)
     pro_description = models.TextField(null=True)
-    quatity = models.PositiveIntegerField(null=True)
+    quantity = models.PositiveIntegerField(null=True)
     price = models.PositiveIntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     product_cat = models.ForeignKey(Category, related_name='product', blank=True, null=True, on_delete=models.CASCADE)
