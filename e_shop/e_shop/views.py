1887ca11863ad4317c93ff28611cef1efbf82a7c
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-27 02:50:26 +0600

    modify views for category list in drop-down navbar

--- e_shop/e_shop/views.py
+++ e_shop/e_shop/views.py
@@ -1,13 +1,19 @@
-from django.views.generic import TemplateView
+from django.views import generic
 from category.models import Category
 
-class HomePage(TemplateView):
+class HomePage(generic.TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        model_cat = Category
-        query = Category.objects.all()
-        print(query)
-        context['category_list'] = query
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['category_list'] = Category.objects.all()
+    #     return context
+
+
+# class CatView(generic.ListView):
+#     model = Category
+#     template_name = 'base.html'
+#     context_object_name = 'category_list'
+
+#     def get_queryset(self):
+#         return Category.objects.all()
