3effeed2f77ab566f0019d3cdef679f66be4f726
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-27 02:51:07 +0600

    add views to for product list and product details into app 'product'

--- e_shop/product/views.py
+++ e_shop/product/views.py
@@ -8,3 +8,13 @@ from category.models import Category
 
 class ProductList(generic.ListView):
     model = Product
+    template_name = 'product/product_list.html'
+
+
+class ProductDetail(generic.DetailView):
+    model = Product
+    template_name = 'product/product_detail.html'
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     return queryset.filter(pk=self.kwargs.get('pk'))
