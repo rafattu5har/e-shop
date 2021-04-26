204c4fb48cb4589c5830f556ab288f8002d88619
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-27 02:53:48 +0600

    modify urls of the app 'product' for created views

--- e_shop/product/urls.py
+++ e_shop/product/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -22,5 +23,6 @@ from django.conf import settings
 app_name = 'product'
 
 urlpatterns = [
-
+    path('', views.ProductList.as_view(), name='all'),
+    url('(?P<pk>\d+)/$', views.ProductDetail.as_view(), name='single'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
