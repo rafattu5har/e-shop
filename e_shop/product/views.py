5c293387dd3ad9561f9d11ffc17cb467326f62b7
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-26 23:59:06 +0600

    add a list-views to product

--- e_shop/product/views.py
+++ e_shop/product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Product
+from category.models import Category
 
 # Create your views here.
+
+class ProductList(generic.ListView):
+    model = Product
