8bf8dd343509e869a233fbff0f508f565d2951a3
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-27 02:49:09 +0600

    add context_processors to the settings.py

--- e_shop/e_shop/settings.py
+++ e_shop/e_shop/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'e_shop.context_processors.cat_list', # for category listing in all the pages with base.html
             ],
         },
     },
