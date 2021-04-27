2a2024012ff7ee9af57460a90dc93d0bbf748252
Rafat Rahman Tushar <rafat.tushar@gmail.com>
2021-04-28 02:47:50 +0600

    modifying product model for unique file name

--- e_shop/product/models.py
+++ e_shop/product/models.py
@@ -3,14 +3,17 @@ from django.urls import reverse
 from category.models import Category
 
 # Create your models here.
-
+def upload_location(instance, filename):
+    filebase, extension = filename.split('.')
+    return 'product_image/' + str(instance.created_at) + '.' + extension
+  
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='product_image', blank=True)
     pro_description = models.TextField(null=True)
     quantity = models.PositiveIntegerField(null=True)
     price = models.PositiveIntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=upload_location, blank=True)
     product_cat = models.ForeignKey(Category, related_name='product', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -18,3 +21,4 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product:single', kwargs={'pk':self.pk})
+
